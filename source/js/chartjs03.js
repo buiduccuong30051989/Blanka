4064e47bec1498a3e679029903d18200c371640f
Cuong <cuong@Cuongs-MacBook-Pro.local>
2019-12-22 21:13:00 +0700

    WIP

---
+++ source/js/chartjs03.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  "use strict";
+
+  var ctx = document.getElementById("myChart");
+  var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      labels: ['OK', 'WARNING', 'CRITICAL'],
+      datasets: [{
+        label: '# of Tomatoes',
+        data: [12, 19, 8],
+        backgroundColor: [
+          '#3fa037',
+          '#5472d2',
+          '#f44336'
+        ]
+      }]
+    },
+    options: {
+      responsive: false,
+      legend: {
+        position: 'bottom',
+        align: 'center',
+        labels: {
+          padding: 30
+        }
+      }
+    }
+  });
+});
