4c81181dc7f34c12b064ea1b9ecbbbeb482fd464
Cuong <cuong@Cuongs-MacBook-Pro.local>
2019-12-22 21:13:00 +0700

    WIP

---
+++ source/js/chartjs04.js
@@ -0,0 +1,91 @@
+$(document).ready(function() {
+  "use strict";
+
+  // google.charts.load("current", { packages: ["corechart", "line"] });
+  // google.charts.setOnLoadCallback(drawChart);
+  // function drawChart() {
+  //   var data = new google.visualization.DataTable();
+  //   data.addColumn("number", "Day");
+  //   data.addColumn("number", "Guardians of the Galaxy");
+  //   data.addColumn("number", "The Avengers");
+  //   data.addColumn("number", "Transformers: Age of Extinction");
+
+  //   data.addRows([
+  //     [1, 37.8, 80.8, 41.8],
+  //     [2, 30.9, 69.5, 32.4],
+  //     [3, 25.4, 57, 25.7],
+  //     [4, 11.7, 18.8, 10.5],
+  //     [5, 11.9, 17.6, 10.4],
+  //     [6, 8.8, 13.6, 7.7],
+  //     [7, 7.6, 12.3, 9.6],
+  //     [8, 12.3, 29.2, 10.6],
+  //     [9, 16.9, 42.9, 14.8],
+  //     [10, 12.8, 30.9, 11.6],
+  //     [11, 5.3, 7.9, 4.7],
+  //     [12, 6.6, 8.4, 5.2],
+  //     [13, 4.8, 6.3, 3.6],
+  //     [14, 4.2, 6.2, 3.4]
+  //   ]);
+
+  //   var options = {
+  //     chart: {
+  //       title: "Box Office Earnings in First Two Weeks of Opening",
+  //       subtitle: "in millions of dollars (USD)",
+  //       legend: {
+  //         position: "bottom"
+  //       }
+  //     }
+  //   };
+
+  //   var chart = new google.charts.Line(document.getElementById("myChart2"));
+  //   chart.draw(data, google.charts.Line.convertOptions(options));
+  // }
+
+  // $(window).resize(function() {
+  //   drawChart();
+  // });
+
+  google.load("visualization", "1", {
+    packages: ["corechart", "line"]
+  });
+  google.setOnLoadCallback(drawBackgroundColor);
+
+  function drawBackgroundColor() {
+    var data = new google.visualization.DataTable();
+    data.addColumn("number", "X");
+    data.addColumn("number", "Compute Time");
+    data.addColumn("number", "Compute Times");
+    data.addColumn("number", "Compute Times");
+    data.addRows([
+      [1, 37.8, 80.8, 41.8],
+      [2, 30.9, 69.5, 32.4],
+      [3, 25.4, 57, 25.7],
+      [4, 11.7, 18.8, 10.5],
+      [5, 11.9, 17.6, 10.4],
+      [6, 8.8, 13.6, 7.7],
+      [7, 7.6, 12.3, 9.6],
+      [8, 12.3, 29.2, 10.6],
+      [9, 16.9, 42.9, 14.8],
+      [10, 12.8, 30.9, 11.6],
+      [11, 5.3, 7.9, 4.7],
+      [12, 6.6, 8.4, 5.2],
+      [13, 4.8, 6.3, 3.6],
+      [14, 4.2, 6.2, 3.4]
+    ]);
+    var options = {
+      legend: {
+        position: "bottom"
+      },
+      backgroundColor: "#f8f8f8"
+    };
+
+    function resize() {
+      var chart = new google.visualization.LineChart(
+        document.getElementById("myChart2")
+      );
+      chart.draw(data, options);
+    }
+    window.onload = resize();
+    window.onresize = resize;
+  }
+});
