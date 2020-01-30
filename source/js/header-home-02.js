90b4741d635d3ac2796a55afc1a59a038b4912d8
bui.duc.cuong <bui.duc.cuong@TrueSign.local>
2020-01-30 16:48:02 +0700

    WIP

---
+++ source/js/header-home-02.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+  "user strict";
+  function initHeader() {
+    var stickyHeader = $("#js-header-home-02");
+    var headerWidth = stickyHeader.width();
+    var headerHeight = stickyHeader.outerHeight(true);
+    if (!window.mobileAndTabletcheck() || $(window).width() > 991) {
+      if (stickyHeader.length > 0) {
+        if ($(".wrap-sticky").length) {
+          var headerWidth2 = $(".ts-header").width();
+          var headerHeight2 = $(".ts-header").outerHeight(true);
+          $(".wrap-sticky").css({
+            width: headerWidth2,
+            height: headerHeight2
+          });
+        } else {
+          stickyHeader.wrap("<div class='wrap-sticky'></div>");
+          $(".wrap-sticky").css({
+            width: headerWidth,
+            height: headerHeight
+          });
+        }
+
+        $(window).scroll(function() {
+          var windowScroll = $(window).scrollTop();
+          if (windowScroll > 37) {
+            stickyHeader.addClass("fixed-top");
+          } else {
+            stickyHeader.removeClass("fixed-top");
+          }
+        });
+      }
+    } else {
+      if ($(".wrap-sticky").length) {
+        var headerWidth2 = $(".ts-header").width();
+        var headerHeight2 = $(".ts-header").outerHeight(true);
+        $(".wrap-sticky").css({
+          width: headerWidth2,
+          height: headerHeight2
+        });
+      } else {
+        stickyHeader.wrap("<div class='wrap-sticky'></div>");
+        $(".wrap-sticky").css({
+          width: headerWidth,
+          height: headerHeight
+        });
+      }
+    }
+  }
+
+  initHeader();
+
+  $(window).on("resize", function() {
+    initHeader();
+  });
+});
---
+++ source/js/header-home-03.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  "user strict";
+
+  var stickyHeader = $("#js-header-home-03");
+  function initHeader() {
+    if (!window.mobileAndTabletcheck() || $(window).width() > 991) {
+      if (stickyHeader.length > 0) {
+        $(window).scroll(function() {
+          var headerContainerHeight = $(".header-container").outerHeight(true);
+          var headerHeight = stickyHeader.outerHeight(true);
+          var windowScroll = $(window).scrollTop();
+          if (windowScroll > headerContainerHeight - headerHeight) {
+            stickyHeader.addClass("sticky-header");
+          } else {
+            stickyHeader.removeClass("sticky-header");
+          }
+        });
+      }
+    }
+  }
+
+  initHeader();
+
+  $(window).on("resize", function() {
+    initHeader();
+  });
+});
