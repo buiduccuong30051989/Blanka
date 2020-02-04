1d041c7bf05f04c96abbd3a461c656e7cd639738
bui.duc.cuong <bui.duc.cuong@TrueSign.local>
2020-02-04 16:39:52 +0700

    WIP

--- source/js/slider-home-02.js
+++ source/js/slider-home-02.js
@@ -1,50 +1,27 @@
 $(document).ready(function () {
-  'user strict';
+  'use strict';
 
-  var $carouselIntro = $('.js-owl-intro');
-
-  if ($carouselIntro.length > 0) {
-    $carouselIntro.each(function () {
-      var owl = $(this);
-
-      function setAnimation(_elem, _InOut) {
-        var animationEndEvent = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-
-        _elem.each(function () {
-          var $elem = $(this);
-          var $animationType = 'animated ' + $elem.data('animation-' + _InOut);
-          var $animationDelay = $elem.data('animation-delay');
-          $(this).css('animation-delay', $animationDelay);
-          $elem.addClass($animationType).one(animationEndEvent, function () {
-            $elem.removeClass($animationType); // remove animate.css Class at the end of the animations
-          });
-        });
-      }
-
-      owl.on('initialize.owl.carousel initialized.owl.carousel ' + 'initialize.owl.carousel initialize.owl.carousel ', function (event) {
-        var $currentItem = $('.owl-item', owl).eq(event.item.index);
-        var $elemsToanim = $currentItem.find('[data-animation-in]');
-        setAnimation($elemsToanim, 'in');
+  var tpj = jQuery;
+  var revapi8;
+  tpj(document).ready(function () {
+    if (tpj('#js-slider-home-02').revolution == undefined) {
+      revslider_showDoubleJqueryError('#js-slider-home-02');
+    } else {
+      revapi8 = tpj('#js-slider-home-02').show().revolution({
+        sliderType: 'standard',
+        jsFileLocation: '../revolution-slider/revolution/js/',
+        sliderLayout: 'fullwidth',
+        delay: 9000,
+        navigation: {
+          arrows: {
+            enable: true,
+            style: 'uranus'
+          }
+        },
+        responsiveLevels: [1920, 992, 768, 576],
+        gridwidth: [1200, 600, 600, 300],
+        gridheight: [590, 400, 400, 400]
       });
-      owl.owlCarousel({
-        loop: true,
-        margin: 0,
-        navSpeed: 500,
-        nav: true,
-        items: 1,
-        animateIn: 'fadeIn',
-        autoplay: true,
-        autoplayTimeout: 9000,
-        mouseDrag: false,
-        touchDrag: false,
-        autoplayHoverPause: true,
-        navText: ['<i class="la la-angle-left"></i>', '<i class="la la-angle-right"></i>']
-      });
-      owl.on('changed.owl.carousel', function (event) {
-        var $currentItem = $('.owl-item', owl).eq(event.item.index);
-        var $elemsToanim = $currentItem.find('[data-animation-in]');
-        setAnimation($elemsToanim, 'in');
-      });
-    });
-  }
+    }
+  });
 });
