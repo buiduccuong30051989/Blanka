eaa8d055b771b7b040e685138edd63731e04aa9d
Cuong <cuong@Cuongs-MacBook-Pro.local>
2020-03-16 11:14:50 +0700

    Fix barnch Demo 02

--- source/js/detech-mobile.js
+++
@@ -1,8 +0,0 @@
-window.mobileAndTabletcheck = function() {
-  var isMobile = false; //initiate as false
-  // device detection
-  if (/Mobi|Android/i.test(navigator.userAgent)) {
-    isMobile = true;
-  }
-  return isMobile;
-};
